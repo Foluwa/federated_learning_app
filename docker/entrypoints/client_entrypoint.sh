d35393c1f03777466d90e38983c59e1af73fcf40
foluwa <moronfoluwaakintola@gmail.com>
2025-08-13 21:15:33 +0100

    Minor changes

--- docker/entrypoints/client_entrypoint.sh
+++ docker/entrypoints/client_entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "Starting Flower client with ID $CLIENT_ID..."
-python3 -m adaptive_federated_healthcare.client.app
+
+# Extract command-line arguments
+SERVER_ADDRESS=$1
+CLIENT_ID=$2
+
+# Execute the Flower client application with the provided arguments
+python3 -m adaptive_federated_healthcare.client.app --server-address "$SERVER_ADDRESS" --cid "$CLIENT_ID"
